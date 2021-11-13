dff8377dfe0b3d153f9c76009ae53e1e33b51a34
Blake Eriks <eblake98@vt.edu>
2021-11-13 06:55:00 -1000

    Update Header with user info

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,21 @@
+import useUserActions from '../services/userActions'
+import { useAuthState } from '../state/Auth'
 import { Button } from '../styles/Button'
 import { HeaderContainer, HeaderContent, HeaderTitle } from  '../styles/Header'
 
 const Header = () => {
+
+    const userActions = useUserActions()
+    const [auth, setAuth] = useAuthState()
+
     return (
         <HeaderContainer>
             <HeaderTitle>
                 bookmarkd
             </HeaderTitle>
             <HeaderContent>
-                Hi User!
-                <Button>
+                Hi {auth.username}!
+                <Button onClick={userActions.logout}>
                     Sign Out
                 </Button>
             </HeaderContent>
