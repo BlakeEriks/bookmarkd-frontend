1f92ea4b2630ee93fda8c6c7ef919bf0bfbc6929
Blake Eriks <eblake98@vt.edu>
2021-11-13 06:55:36 -1000

    Linking login and signup pages to backend

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,22 +1,51 @@
+import { useState } from "react"
+import { Navigate } from "react-router"
+import useUserActions from "../services/userActions"
+import { useAuthState } from "../state/Auth"
+
 const SignUp = () => {
+
+    const userActions = useUserActions()
+    const [form, setForm] = useState({username: '', password: ''})
+    const [err, setErr] = useState(false)
+    const [auth] = useAuthState()
+
+    const handleSubmit = async event => {
+        event.preventDefault()
+        try {
+            await userActions.register({...form})
+        }
+        catch (err) {
+            setErr(true)
+        }
+    }
+
+    const onChange = event => {
+        setForm({...form, [event.target.name]: event.target.value})
+    }
+
     return (
+        auth ? <Navigate to="/home" /> :
         <div>
-            <form action="/signup" method="post">
+            <form action="/signup" method="post" onSubmit={handleSubmit}>
                 <fieldset>
                 <legend>Sign Up Here</legend>
 
                 <label>
-                    Desired Username: <input type="text" name="username" placeholder="username" required></input>
+                    Desired Username: <input type="text" name="username" value={form.username} placeholder="username" onChange={onChange} required></input>
                 </label>
 
                 <label>
-                    Password: <input type="password" name="password" placeholder= "password" required></input>
+                    Password: <input type="password" name="password" value={form.password} placeholder= "password" onChange={onChange} required></input>
                 </label>
 
                 <input type="submit" value="Create Account"></input>
                 </fieldset>
+                {err &&
+                <div>
+                    Username taken. Please pick another.
+                </div>}
             </form>
-
         </div>
     )
 }
