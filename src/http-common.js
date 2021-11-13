c67e5694978f48c9bb00769c8d6ef1f3fb526a70
alextakada322 <88411843+alextakada322@users.noreply.github.com>
2021-11-12 17:57:00 -0800

    Update http-common.js

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: '',
+    baseURL: 'https://jab-bookmarkd-backend.herokuapp.com/',
     headers: {
         'Content-type': 'application/json'
     }
