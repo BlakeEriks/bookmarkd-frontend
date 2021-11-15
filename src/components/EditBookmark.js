aaebe1817d43f587cc38b0272d721bf0fd556049
Blake Eriks <eblake98@vt.edu>
2021-11-14 22:39:05 -1000

    Add form state and submit function

--- src/components/EditBookmark.js
+++ src/components/EditBookmark.js
@@ -1,19 +1,34 @@
-import { HorizontalFlexBox, VerticalFlexBox } from "../styles/Boxes"
+import { useState } from "react"
+import { HorizontalFlexBox, HorizontalFlexBoxForm, VerticalFlexBox } from "../styles/Boxes"
 import { Button, EditAddButton } from "../styles/Button"
 import { PlusIcon } from "../styles/Icons"
 import { Input } from "../styles/Input"
 
-const EditBookmark = () => {
+const EditBookmark = ({createBookmark}) => {
+
+    const [form, setForm] = useState({name: '', url: ''})
+
+    const onChange = event => {
+        setForm({...form, [event.target.name]: event.target.value})
+    }
+
+    const handleSubmit = event => {
+        event.preventDefault()
+        createBookmark(form)
+    }
+
     return (
-        <HorizontalFlexBox>
-            <VerticalFlexBox>
-                <Input placeholder='Website'/>
-                <Input placeholder='Link' />
-            </VerticalFlexBox>
-            <EditAddButton>
-                <PlusIcon />
-            </EditAddButton>
-        </HorizontalFlexBox>
+        <form onSubmit={handleSubmit}>
+            <HorizontalFlexBox>
+                <VerticalFlexBox>
+                    <Input onChange={onChange} value={form.name} name='name' placeholder='Website'/>
+                    <Input onChange={onChange} value={form.url} name='url' placeholder='Link' />
+                </VerticalFlexBox>
+                <EditAddButton>
+                    <PlusIcon />
+                </EditAddButton>
+            </HorizontalFlexBox>
+        </form>
     )
 }
 
