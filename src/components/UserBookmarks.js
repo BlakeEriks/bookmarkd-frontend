32c3e9d3e95b3a4246dc0f305871640a32e7df2f
Blake Eriks <eblake98@vt.edu>
2021-11-14 22:39:53 -1000

    Add backend call for getting bookmarks, add create function

--- src/components/UserBookmarks.js
+++ src/components/UserBookmarks.js
@@ -1,19 +1,39 @@
+import { useEffect, useState } from "react"
+import useBookmarks from "../services/bookmarks"
+import { useAuthState } from "../state/Auth"
 import { VerticalFlexBox } from "../styles/Boxes"
 import Bookmark from "./Bookmark"
 import EditBookmark from "./EditBookmark"
 import SearchBar from "./SearchBar"
 
 const UserBookmarks = () => {
+
+    const [auth] = useAuthState()
+    const bookmarkService = useBookmarks()
+    const [bookmarks, setBookmarks] = useState([])
+
+    useEffect( () => {
+        bookmarkService.get().then(res => {
+            console.log(auth)
+            console.log(res)
+            setBookmarks(res)
+        })
+    }, ([]))
+
+    const createBookmark = bookmark => {
+        bookmarkService.create(bookmark)
+            .then( res => {
+                setBookmarks([...bookmarks, res])
+            })
+    }
+
     return (
         <VerticalFlexBox width='60%'>
             <SearchBar />
             <ul>
-                <Bookmark website={'CodeWars'} />
-                <Bookmark website={'React Docs'} />
-                <Bookmark website={'LinkedIn'} />
-                <Bookmark website={'Twitter'} />
+                {bookmarks.map( bookmark => <Bookmark key={bookmark._id} {...bookmark} />)}
             </ul>
-            <EditBookmark />
+            <EditBookmark createBookmark={createBookmark}/>
         </VerticalFlexBox>
     )
 }
