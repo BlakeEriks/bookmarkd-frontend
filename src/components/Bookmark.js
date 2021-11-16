0a4452996b0dddb8c785a6b780541c2c6a7d39be
Blake Eriks <eblake98@vt.edu>
2021-11-15 19:24:09 -1000

    Add edit and delete buttons + functionality

--- src/components/Bookmark.js
+++ src/components/Bookmark.js
@@ -1,12 +1,24 @@
+import { HorizontalFlexBox } from "../styles/Boxes"
+import { Button } from "../styles/Button"
+import { EditIcon, TimesIcon } from "../styles/Icons"
 import { Text } from "../styles/Text"
 
-const Bookmark = ({name, url}) => {
+const Bookmark = ({_id, name, url, removeBookmark, setForm}) => {
     return (
-        <Text>
+        <HorizontalFlexBox style={{marginBottom: '3px'}}>
             <a href={'https://' + url} target='_blank'>
-                {name}
-            </a>    
-        </Text>
+                <Text>{name}</Text>
+            </a>
+            <HorizontalFlexBox>
+                <Button width='30px' padding='none' onClick={() => removeBookmark(_id)}>
+                    <TimesIcon />
+                </Button>
+                <Button width='30px' padding='none' onClick={() => setForm({name, url})}>
+                    <EditIcon />
+                </Button>
+            </HorizontalFlexBox>
+        </HorizontalFlexBox>
+        
     )
 }
 
