627a872246ac7561345977cf8ea6c59200616025
Blake Eriks <eblake98@vt.edu>
2021-11-15 19:26:23 -1000

    Add useful icons

--- src/styles/Icons.js
+++ src/styles/Icons.js
@@ -1,4 +1,4 @@
-import { Plus, Search } from "@styled-icons/fa-solid";
+import { Check, Edit, Plus, Search, Times } from "@styled-icons/fa-solid";
 import styled from "styled-components";
 import { colors } from "./Config";
 
@@ -13,5 +13,26 @@ export const PlusIcon = styled(Plus)`
     color: ${colors.light};
     width: 32px;
     height: 32px;
+    transition: color 0.3s ease-in-out;
+`
+
+export const CheckIcon = styled(Check)`
+    color: ${colors.light};
+    width: 32px;
+    height: 32px;
+    transition: color 0.3s ease-in-out;
+`
+
+export const TimesIcon = styled(Times)`
+    color: ${colors.light};
+    width: 20px;
+    height: 20px;
+    transition: color 0.3s ease-in-out;
+`
+
+export const EditIcon = styled(Edit)`
+    color: ${colors.light};
+    width: 20px;
+    height: 20px;
     transition: color 0.3s ease-in-out;
 `
