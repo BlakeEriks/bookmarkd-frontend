4035eb8ff36ad1d3df8bf36083ed9f962cce03e4
Blake Eriks <eblake98@vt.edu>
2021-11-17 21:09:12 -1000

    Fixing user authentication

--- src/services/userActions.js
+++ src/services/userActions.js
@@ -1,31 +1,39 @@
 import useHttp from "../http";
-import { useAuthState } from "../state/Auth";
+import { useAuthState, useUserState } from "../state/Auth";
 
 const useUserActions = () => {
     
     const http = useHttp()
     const [auth, setAuth] = useAuthState()
+    const [user, setUser] = useUserState()
     const API_BASE_URL = process.env.REACT_APP_API_URL
     
     const login = ({ username, password }) => {
         return http.post(`${API_BASE_URL}/authenticate`, {username, password})
-            .then( user => {
-                localStorage.setItem('user', JSON.stringify(user));
-                setAuth(user)
+            .then( data => {
+                const {token, username} = data
+                localStorage.setItem('token', JSON.stringify(token));
+                localStorage.setItem('username', JSON.stringify(username));
+                setAuth(token)
+                setUser(username)
             })
     }
 
     const register = ({ username, password }) => {
         return http.post(`${API_BASE_URL}/register`, { username, password })
-            .then( user => {
-                localStorage.setItem('user', JSON.stringify(user));
-                setAuth(user)
+            .then( data => {
+                const {token, username} = data
+                localStorage.setItem('token', JSON.stringify(token));
+                localStorage.setItem('username', JSON.stringify(username));
+                setAuth(token)
+                setUser(username)
             })
     }
 
     const logout = () => {
         http.get(`${API_BASE_URL}/logout`)
-        localStorage.removeItem('user');
+        localStorage.removeItem('token');
+        localStorage.removeItem('username');
         setAuth(null)
     }
 
--- src/state/Auth.js
+++ src/state/Auth.js
@@ -1,11 +1,21 @@
 import { atom, useRecoilState } from 'recoil'
 
 const authState = atom({
-    key: 'auth',
+    key: 'token',
     // get initial state from local storage to enable user to stay logged in
-    default: JSON.parse(localStorage.getItem('user'))
+    default: JSON.parse(localStorage.getItem('token'))
+})
+
+const userState = atom({
+    key: 'user',
+    // get initial state from local storage to enable user to stay logged in
+    default: JSON.parse(localStorage.getItem('username'))
 })
 
 export const useAuthState = () => {
     return useRecoilState(authState)
+}
+
+export const useUserState = () => {
+    return useRecoilState(userState)
 }
